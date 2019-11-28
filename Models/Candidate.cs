99c1b0f077134dd925e8a2e8600e142f8fe0cafe
Ben Tilghman <ben.tilghman@gmail.com>
2019-11-27 23:00:22 -0500

    added dropdown menu for parties

--- Models/Candidate.cs
+++ Models/Candidate.cs
@@ -10,6 +10,7 @@ namespace RazorCandidate.Models
     public string Last { get; set; }
     public string First { get; set; }
 
+    [Display(Name = "Announce Date")]
     [DataType(DataType.Date)]
     public DateTime AnnounceDate { get; set; }
     public string Party { get; set; }
--- Pages/Candidates/Index.cshtml.cs
+++ Pages/Candidates/Index.cshtml.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using RazorCandidate.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace candidate.Pages_Candidates
 {
@@ -19,10 +20,27 @@ namespace candidate.Pages_Candidates
         }
 
         public IList<Candidate> Candidate { get;set; }
+        
+        public SelectList Parties { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string CandidateParties { get; set; }
 
         public async Task OnGetAsync()
         {
-            Candidate = await _context.Candidate.ToListAsync();
+            IQueryable<string> partyQuery = from m in _context.Candidate
+                                            orderby m.Party
+                                            select m.Party;
+
+            var candidates = from m in _context.Candidate
+                        select m;
+            
+            if (!string.IsNullOrEmpty(CandidateParties))
+            {
+              candidates = candidates.Where(x => x.Last == CandidateParties);
+            }
+            Parties = new SelectList(await partyQuery.Distinct().ToListAsync());
+            
+            Candidate = await candidates.ToListAsync();
         }
     }
 }
